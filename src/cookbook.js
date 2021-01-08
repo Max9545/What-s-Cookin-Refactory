9f69f3ec2ca618c709121d875631102a1e8ccbbc
Max Bregman <bregmanmax91@gmail.com>
2021-01-07 18:48:05 -0700

    pass  check pantry test for single ingredient search

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,16 +1,18 @@
 class Cookbook {
-  constructor(data) {
-    this.recipes = data;
-  }
+    constructor(data) {
+        this.recipes = data;
+    }
 
-  findRecipe(searchText) {
-    return this.recipes.filter(recipe => {
-      return recipe.ingredients.find(ingredient => {
-        return (ingredient.name.includes(searchText)) ||
-        (recipe.name.includes(searchText))
-      });
-    })
-  }
+    findRecipe(searchText) {
+
+        //this looks weird
+        return this.recipes.filter(recipe => {
+            return recipe.ingredients.find(ingredient => {
+                return (ingredient.name.includes(searchText)) ||
+                    (recipe.name.includes(searchText))
+            });
+        })
+    }
 }
 
 export default Cookbook;
--- src/user.js
+++ src/user.js
@@ -1,37 +1,42 @@
 class User {
-  constructor(id, name, pantry) {
-    this.id = id;
-    this.name = name;
-    this.pantry = pantry;
-    this.favoriteRecipes = [];
+    constructor(id, name, pantry) {
+        this.id = id;
+        this.name = name;
+        this.pantry = pantry;
+        this.favoriteRecipes = [];
 
-  }
-
-  addToFavorites(recipe) {
-    if (!this.favoriteRecipes.includes(recipe)) {
-      this.favoriteRecipes.push(recipe)
     }
-  }
 
-  removeFromFavorites(recipe) {
-    const i = this.favoriteRecipes.indexOf(recipe);
-    this.favoriteRecipes.splice(i, 1)
-  }
+    addToFavorites(recipe) {
+        if (!this.favoriteRecipes.includes(recipe)) {
+            this.favoriteRecipes.push(recipe)
+        }
+    }
 
-  filterFavorites(tag) {
-    return this.favoriteRecipes.filter(recipe => {
-      return recipe.tags.includes(tag);
-    });
-  }
+    removeFromFavorites(recipe) {
+        const i = this.favoriteRecipes.indexOf(recipe);
+        this.favoriteRecipes.splice(i, 1)
+    }
 
-  findFavorites(strgToSrch) {
-    return this.favoriteRecipes.filter(recipe => {
-      return recipe.name.includes(strgToSrch)
-      || recipe.ingredients.find(ingredient => {
-        return ingredient.name.includes(strgToSrch)
-      });
-    });
-  }
+    filterFavorites(tag) {
+        return this.favoriteRecipes.filter(recipe => {
+            return recipe.tags.includes(tag);
+        });
+    }
+
+    findFavorites(strgToSrch) {
+        return this.favoriteRecipes.filter(recipe => {
+            return recipe.name.includes(strgToSrch) ||
+                recipe.ingredients.find(ingredient => {
+                    return ingredient.name.includes(strgToSrch)
+                });
+        });
+    }
+    checkPantry(ingredientToFind) {
+        if (this.pantry.find(ingredient => ingredient.ingredient === ingredientToFind.ingredient)) {
+            return 'You have the ingredients!'
+        }
+    }
 }
 
 
