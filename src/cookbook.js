38fe79fee979c00a8dde1ab807c73c36242f6199
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-11 15:24:28 -0500

    Pseudo coding for refactoring

--- src/cookbook.js
+++ src/cookbook.js
@@ -4,15 +4,28 @@ class Cookbook {
     }
 
   findRecipe(searchText) {
-    return this.recipes.filter(recipe => {
-      return recipe.ingredients.find(ingredient => {
-        return recipe.tags.find(tag => {
-          return (ingredient.name.includes(searchText) ||
-            recipe.name.includes(searchText) || tag.includes(searchText))
+    //include a conditional to circumnavigate problem, maybe?
+    //if it's a tag
+    //if it's a name(partial or otherwise)
+    //if it's an ingredient
+
+
+  //find a recipe with that searchText as its name
+
+  //if that fails, then, filter recipes with that searchText in the ingredients
+  //if that fails, then filter recipes with that searchText in the tags
+  //if that fails: alert, sorry or something like that.
+      const searchResult = this.recipes.filter(recipe => {
+       return recipe.ingredients.find(ingredient => {
+        return  recipe.tags.find(tag => {
+          return (recipe.name.includes (searchText) || ingredient.name.includes(searchText) || tag.includes(searchText))
         });
       })
     })
+    return searchResult;
   }
+
+  // findByTag
 }
 
 export default Cookbook;
