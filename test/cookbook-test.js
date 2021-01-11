81149b7fc60e4ec9b58ccf1f64e03e2a5db882d9
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-11 15:25:57 -0500

    Debug process of class and scripts

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -7,7 +7,7 @@ import recipeData from '../src/data/recipes';
 import Cookbook from '../src/cookbook';
 
 
-describe('Cookbook', () => {
+describe.only('Cookbook', () => {
   let cookbook;
 
   beforeEach(() => {
@@ -21,18 +21,22 @@ describe('Cookbook', () => {
 
   describe('findRecipe', () => {
     it('Should be able to filter through its array by ingredients', () => {
+      console.log(cookbook.findRecipe('yolk'))
       expect(cookbook.findRecipe('yolk').length).to.equal(2);
     });
 
     it('Should be able to filter through its array by name', () => {
+      console.log(cookbook.findRecipe('Sesame Cookies'));
       expect(cookbook.findRecipe('Sesame Cookies')[0]).to.deep.equal(recipeData[7]);
     });
 
     it('Should be able to filter through its array by tag', () => {
+      console.log(cookbook.findRecipe('starter'))
       expect(cookbook.findRecipe('starter').length).to.equal(9);
     })
 
     it('Should have no recipies with the same name', () => {
+      console.log(cookbook.findRecipe('Loaded Chocolate Chip Pudding Cookie Cups'))
       expect(cookbook.findRecipe('Loaded Chocolate Chip Pudding Cookie Cups').length).to.equal(1);
     })
   });
