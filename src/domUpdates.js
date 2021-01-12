d7e16f11587002baa3d591dbc1aa0f233d1da543
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-11 19:26:27 -0500

    Include conditional for object or an array

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,24 +2,45 @@ let domUpdates = {
   displayCards(recipesList, cardArea) {
    cardArea.classList.remove('all')
    cardArea.innerHTML = '';
-   recipesList.forEach(recipe => {
-     cardArea.insertAdjacentHTML('afterbegin', `<div id='${recipe.id}'
-     class='card'>
-     <header id='${recipe.id}' class='card-header'>
-     <label for='add-button' class='hidden'>Click to add recipe</label>
-     <button id='${recipe.id}'  class='add-button card-button'>
-     <img id='${recipe.id}' class='add'
-     src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
-     recipes to cook'></button>
-     <label for='favorite-button' class='hidden'>Click to favorite recipe
-     </label>
-     <button id='${recipe.id}'class='favorite favorite${recipe.id} card-button'>
-     </button></header>
-     <img id='${recipe.id}' tabindex='0' class='card-picture'
-     src='${recipe.image}' alt='Food from recipe'>
-     <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
-     </div>`)
+   if (Array.isArray(recipesList)) {
+     recipesList.forEach(recipe => {
+       cardArea.insertAdjacentHTML('afterbegin', `<div id='${recipe.id}'
+       class='card'>
+       <header id='${recipe.id}' class='card-header'>
+       <label for='add-button' class='hidden'>Click to add recipe</label>
+       <button id='${recipe.id}'  class='add-button card-button'>
+       <img id='${recipe.id}' class='add'
+       src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
+       recipes to cook'></button>
+       <label for='favorite-button' class='hidden'>Click to favorite recipe
+       </label>
+       <button id='${recipe.id}'class='favorite favorite${recipe.id} card-button'>
+       </button></header>
+       <img id='${recipe.id}' tabindex='0' class='card-picture'
+       src='${recipe.image}' alt='Food from recipe'>
+       <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+       </div>`)
    })
+ } else {
+    const recipe = recipesList;
+
+    cardArea.innerHTML = `<div id='${recipe.id}'
+      class='card'>
+      <header id='${recipe.id}' class='card-header'>
+      <label for='add-button' class='hidden'>Click to add recipe</label>
+      <button id='${recipe.id}'  class='add-button card-button'>
+      <img id='${recipe.id}' class='add'
+      src='https://image.flaticon.com/icons/svg/32/ 32339.svg' alt='Add to
+      recipes to cook'></button>
+      <label for='favorite-button' class='hidden'>Click to favorite recipe
+      </label>
+      <button id='${recipe.id}'class='favorite favorite${recipe.id} card-button'>
+      </button></header>
+      <img id='${recipe.id}' tabindex='0' class='card- picture'
+      src='${recipe.image}' alt='Food from recipe'>
+      <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+      </div>`
+  }
  },
 
  greetUser(user) {
@@ -42,7 +63,7 @@ let domUpdates = {
   },
 
   findIngredientName(number, info) {
-    const ingredient = info.find(ingredient =>ingredient.id === number)
+    const ingredient = info.find(ingredient => ingredient.id === number)
     return ingredient.name
   },
 
