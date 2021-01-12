47c8f1b619f062760860f5b479ccc3016b01a44f
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-11 19:23:16 -0500

    Refactor class for seaching

--- src/cookbook.js
+++ src/cookbook.js
@@ -4,28 +4,32 @@ class Cookbook {
     }
 
   findRecipe(searchText) {
-    //include a conditional to circumnavigate problem, maybe?
-    //if it's a tag
-    //if it's a name(partial or otherwise)
-    //if it's an ingredient
+    if (this.findRecipeByName(searchText)) {
+      console.log(this.findRecipeByName(searchText))
+      return this.findRecipeByName(searchText)
+    } 
 
+    if (this.findRecipeByTag(searchText)) {
+      return this.findRecipeByTag(searchText)
+    }
 
-  //find a recipe with that searchText as its name
-
-  //if that fails, then, filter recipes with that searchText in the ingredients
-  //if that fails, then filter recipes with that searchText in the tags
-  //if that fails: alert, sorry or something like that.
-      const searchResult = this.recipes.filter(recipe => {
-       return recipe.ingredients.find(ingredient => {
-        return  recipe.tags.find(tag => {
-          return (recipe.name.includes (searchText) || ingredient.name.includes(searchText) || tag.includes(searchText))
-        });
-      })
-    })
-    return searchResult;
   }
 
-  // findByTag
+  findRecipeByName(searchText) {
+    return this.recipes.find(recipe => recipe.name.includes(searchText))
+  }
+
+  findRecipeByTag(searchText) {
+    return this.recipes.filter(recipe => recipe.tags.find(tag => tag.includes(searchText)));
+    }
+
+  findRecipeByIngredient() {
+
+  }
+
+
+  //totally separate out each function
+  //need helper fx to compare recipes.id and ingreident
 }
 
 export default Cookbook;
