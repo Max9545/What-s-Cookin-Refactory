98453b3bc89cf91da0f210d5ee7138013f590c0b
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-12 17:11:08 -0500

    Update all tests to utlize findRecipe funciton only

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -25,7 +25,11 @@ describe.only('Cookbook', () => {
     it('Should be able to filter through its array by name', () => {
       expect(cookbook.findRecipe('Sesame Cookies')).to.deep.equal(recipeData[7]);
       expect(cookbook.findRecipe('sesame cookies')).to.deep.equal(recipeData[7]);
+    });
 
+    it('Should be able to filter through its array by name and get multiple possiblities', () => {
+      expect(cookbook.findRecipe('cake').length).to.equal(2);
+      expect(cookbook.findRecipe('cake')).to.deep.equal([recipeData[22], recipeData[27]]);
     });
 
     it('Should be able to filter through its array by tag', () => {
@@ -34,8 +38,11 @@ describe.only('Cookbook', () => {
     })
 
     it('Should be able to filter through its array by ingredients', () => {
-      expect(cookbook.findRecipeByIngredient('YOlk', ingredientsData).length).to.equal(2);
-      expect(cookbook.findRecipeByIngredient('yolk', ingredientsData).length).to.equal(2);
+      // console.log(cookbook.findRecipeByIngredient('YOlk', ingredientsData))
+      expect(cookbook.findRecipe('YOlk', ingredientsData).length).to.equal(2);
+      expect(cookbook.findRecipe('yolk', ingredientsData).length).to.equal(2);
+      // expect(cookbook.findRecipeByIngredient('YOlk', ingredientsData).length).to.equal(2);
+      // expect(cookbook.findRecipeByIngredient('yolk', ingredientsData).length).to.equal(2);
     });
   });
 })
