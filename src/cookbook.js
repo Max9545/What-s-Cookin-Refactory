532c362368c3244e30a25932b460ffc2c12eff0d
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-12 17:10:31 -0500

    Update findRecipe to include findRecipeByIngredient helper function

--- src/cookbook.js
+++ src/cookbook.js
@@ -3,20 +3,31 @@ class Cookbook {
     this.recipes = data;
   }
 
-  findRecipe(searchText) {
+  findRecipe(searchText, data) {
     if (this.findRecipeByName(searchText)) {
-      return this.findRecipeByName(searchText);
+      return this.findRecipeByName(searchText)
     } else if (this.findRecipeByTag(searchText)) {
       return this.findRecipeByTag(searchText);
+    } else {
+      return this.findRecipeByIngredient(searchText, data);
     }
+    //now how the fuck do I apply byingredients?
   }
 
   findRecipeByName(searchText) {
-    return this.recipes.find(recipe => recipe.name.includes(this.capitalize(searchText)))
+    const byNames = this.recipes.filter(recipe => recipe.name.includes(this.capitalize(searchText)))
+    if (byNames.length === 1) {
+      return byNames[0];
+    } else if (byNames.length > 1) {
+      return byNames;
+    }
   }
 
   findRecipeByTag(searchText) {
-    return this.recipes.filter(recipe => recipe.tags.find(tag => tag.includes(searchText.toLowerCase())));
+    const byTag = this.recipes.filter(recipe => recipe.tags.find(tag => tag.includes(searchText.toLowerCase())));
+    if (byTag.length > 1) {
+      return byTag;
+    }
   }
 
   findRecipeByIngredient(searchText, data) {
