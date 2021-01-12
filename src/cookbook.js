accdc9fea8256d59877f61aa584e7b0aae5f0df1
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-12 09:30:14 -0500

    Account for capitalization of inputed words

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,32 +1,32 @@
 class Cookbook {
-    constructor(data) {
-        this.recipes = data;
-    }
+  constructor(data) {
+    this.recipes = data;
+  }
 
   findRecipe(searchText) {
-    if (this.findRecipeByName(searchText)) {
-      console.log(this.findRecipeByName(searchText))
-      return this.findRecipeByName(searchText)
-    } 
-
-    if (this.findRecipeByTag(searchText)) {
-      return this.findRecipeByTag(searchText)
+    if (this.findRecipeByName(searchText.capitalize())) {
+      return this.findRecipeByName(searchText);
+    } else if (this.findRecipeByTag(searchText)) {
+      return this.findRecipeByTag(searchText);
     }
 
   }
 
   findRecipeByName(searchText) {
-    return this.recipes.find(recipe => recipe.name.includes(searchText))
+    return this.recipes.find(recipe => recipe.name.includes(capitalize(searchText)))
   }
 
   findRecipeByTag(searchText) {
-    return this.recipes.filter(recipe => recipe.tags.find(tag => tag.includes(searchText)));
-    }
-
-  findRecipeByIngredient() {
-
+    return this.recipes.filter(recipe => recipe.tags.find(tag => tag.includes(searchText.toLowerCase())));
   }
 
+  findRecipeByIngredient() {
+    //include to lowercase
+  }
+
+  capitalize(searchText) {
+    return searchText.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
 
   //totally separate out each function
   //need helper fx to compare recipes.id and ingreident
