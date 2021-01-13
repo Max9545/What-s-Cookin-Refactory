f374c8ca5b5890a73267f7f27949064c837e63dc
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-01-12 20:56:23 -0500

    Refactor tests based on changed made in Cookbook

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -23,26 +23,23 @@ describe.only('Cookbook', () => {
   describe('findRecipe', () => {
 
     it('Should be able to filter through its array by name', () => {
-      expect(cookbook.findRecipe('Sesame Cookies')).to.deep.equal(recipeData[7]);
-      expect(cookbook.findRecipe('sesame cookies')).to.deep.equal(recipeData[7]);
+      expect(cookbook.findRecipe('Sesame Cookies', ingredientsData)).to.deep.equal(recipeData[7]);
+      expect(cookbook.findRecipe('sesame cookies', ingredientsData)).to.deep.equal(recipeData[7]);
     });
 
     it('Should be able to filter through its array by name and get multiple possiblities', () => {
-      expect(cookbook.findRecipe('cake').length).to.equal(2);
-      expect(cookbook.findRecipe('cake')).to.deep.equal([recipeData[22], recipeData[27]]);
+      expect(cookbook.findRecipe('cake', ingredientsData).length).to.equal(2);
+      expect(cookbook.findRecipe('cake', ingredientsData)).to.deep.equal([recipeData[22], recipeData[27]]);
     });
 
     it('Should be able to filter through its array by tag', () => {
-      expect(cookbook.findRecipe('starter').length).to.equal(9);
-      expect(cookbook.findRecipe('Starter').length).to.equal(9);
+      expect(cookbook.findRecipe('starter', ingredientsData).length).to.equal(9);
+      expect(cookbook.findRecipe('Starter', ingredientsData).length).to.equal(9);
     })
 
     it('Should be able to filter through its array by ingredients', () => {
-      // console.log(cookbook.findRecipeByIngredient('YOlk', ingredientsData))
       expect(cookbook.findRecipe('YOlk', ingredientsData).length).to.equal(2);
       expect(cookbook.findRecipe('yolk', ingredientsData).length).to.equal(2);
-      // expect(cookbook.findRecipeByIngredient('YOlk', ingredientsData).length).to.equal(2);
-      // expect(cookbook.findRecipeByIngredient('yolk', ingredientsData).length).to.equal(2);
     });
   });
 })
